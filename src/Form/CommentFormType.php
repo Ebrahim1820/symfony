7a599eddb2b39fa16968e4f52d29fab19a52389a
Ebrahim <naser.najafi1988@gmail.com>
2024-02-05 11:27:00 +0100

    adding the field types and datetime to the field

--- src/Form/CommentFormType.php
+++ src/Form/CommentFormType.php
@@ -3,17 +3,24 @@
 
 namespace App\Form;
 use App\Entity\Comment;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType as SymfonyTextType;
 
 class CommentFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $optopns){
 
         $builder
-            ->add('name')
+            ->add('name', SymfonyTextType::class, [
+                'help'=>'Choose something catche!',
+            ])
             ->add('comment')
+            ->add('commentedAt', null, [
+                'widget'=>'single_text'
+            ])
         ;
 
     }
